b1e1d953042eb401e8ec1283488c75d81b62db6b
devjeffalves <jeff-alves@outlook.com>
2023-12-17 11:35:51 -0300

    Editando

--- calculadora--/src/App.tsx
+++ calculadora--/src/App.tsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/header'
 
 function App() {
  return (
-  <h1>Hello World!</h1>
+ <Header/>
  )
-}
+};
 
 export default App
