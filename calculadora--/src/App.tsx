b109dee98dceb55b3c80cb7bff80fac8a050a5e6
devjeffalves <jeff-alves@outlook.com>
2023-12-26 14:49:06 -0300

    Projeto concluido

--- calculadora--/src/App.tsx
+++ calculadora--/src/App.tsx
@@ -1,16 +1,19 @@
-import './App.css'
-import Header from './components/header'
-import InfoBar from './components/infoBar'
+import './App.css';
+import Header from './components/header';
+import InfoBar from './components/infoBar';
+import CalculatorContainer from './components/CalculatorContainer';
 import Footer from './components/footer';
 
 function App() {
  return (
  <>
  <Header/>
- <InfoBar title="Para você que é MEI, calcule o valor da sua hora de trabalho:"
- />
+ <InfoBar title="Para você que é MEI, calcule o valor da sua hora de trabalho:"/>
 
  <InfoBar title='Calcule o valor a ser cobrado para um projeto:'/>
+
+ <CalculatorContainer />
+
  <Footer/>
  </>
  );
